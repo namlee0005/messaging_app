9b4ca9738008b88468f947d25c02bc39bf302a7d
namkx <namkx020798@gmail.com>
2020-06-23 22:35:21 +0700

    update UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:messaging_app/pages/ConversationPageList.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,31 +15,9 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.yellow,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Namkx'),
+      home: ConversationPageList(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: new Text("Hello World!")
-      ),
-     
-    );
-  }
-}
